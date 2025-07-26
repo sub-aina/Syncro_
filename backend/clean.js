78f31825280370819bb03f14e706a812707f19a4
sub-aina <subainamonib3@gmail.com>
2025-07-26 20:32:19 +0500

    dummy data clean

---
+++ backend/clean.js
@@ -0,0 +1,31 @@
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import User from "./schema/user.js";
+// import Project from "./schema/project.js";
+// import Team from "./schema/team.js";
+// import CheckIn from "./schema/checkIn.js";
+// import Resource from "./schema/Resource.js";
+
+// dotenv.config();
+
+// async function cleanupDB() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log("Connected to MongoDB");
+
+
+//         await User.deleteMany({});
+//         await Project.deleteMany({});
+//         await Team.deleteMany({});
+//         await CheckIn.deleteMany({});
+//         await Resource.deleteMany({});
+
+//         console.log("Dummy data removed ");
+//         process.exit();
+//     } catch (err) {
+//         console.error("Cleanup failed ", err);
+//         process.exit(1);
+//     }
+// }
+
+// cleanupDB();
